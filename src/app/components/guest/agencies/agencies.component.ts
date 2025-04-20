c3644097f1c1f37238767a4303a19541eab83adb
shibil885 <shibilmuhammed885@gmail.com>
2025-04-20 19:49:40 +0530

    fix: new component created

---
+++ src/app/components/guest/agencies/agencies.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-agencies',
+  standalone: true,
+  imports: [],
+  templateUrl: './agencies.component.html',
+  styleUrl: './agencies.component.css'
+})
+export class AgenciesComponent {
+
+}
--- src/app/routes/user.router.ts
+++ src/app/routes/user.router.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { LandingComponent } from '../components/user/landing/landing.component';
+import { LandingComponent } from '../components/guest/landing/landing.component';
 import { LoginComponent } from '../components/user/login/login.component';
 import { HomeComponent } from '../components/user/home/home.component';
 import { SignupComponent } from '../components/user/signup/signup.component';
